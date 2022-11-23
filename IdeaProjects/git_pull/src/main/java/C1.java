fdf69b5b364290b79eb65ba72b4c471112c896c3
FarukDarama <117317450+FarukDarama@users.noreply.github.com>
2022-11-23 20:59:51 +0100

    Update C1.java

--- IdeaProjects/git_pull/src/main/java/C1.java
+++ IdeaProjects/git_pull/src/main/java/C1.java
@@ -2,7 +2,7 @@ public class C1 {
     public static void main(String[] args) {
 
 
-        System.out.println("Hallo");
+        System.out.println("Hallo world");
         //Github ucuncu gun pull yapacagız
 
     }
